ec1a0cfc602a904db8f2639edd87fb241af016ef
Adeleye Ayodeji <biodunhi@gmail.com>
2023-04-21 13:35:53 +0100

    Added update

--- errandlr-delivery-for-woocommerce.php
+++ errandlr-delivery-for-woocommerce.php
@@ -6,8 +6,8 @@
  * Author: Errandlr
  * Author URI: https://errandlr.com/
  * Description: Errandlr Delivery for WooCommerce
- * Version: 1.0.7
- * License: 1.0.7
+ * Version: 1.0.8
+ * License: 1.0.8
  * License URL: http://www.gnu.org/licenses/gpl-2.0.txt
  * text-domain: errandlr-delivery-for-woocommerce
  */
